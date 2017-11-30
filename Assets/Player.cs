c6d46b2c92b25d955c61dec05a96d700c60a0e57
quentinPitalier <quentin.pitalier@gmail.com>
2017-11-30 21:55:22 +0200

    Review of Cloud Techniques for Virtual Reality , code upload
    A commit including all the necessary assets, code and executables for
    running the experiment 1 & 2 of the seminar paper Review of Cloud
    Techniques for Virtual Reality.

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject playerObj;
+    public bool experiment2;
+
+	// Use this for initialization
+	void Start () {
+
+        // Start experiment2 if asked
+        // experiment2 will add the given gameObject after 10seconds and add it again every 10seconds
+        // it simulate the presence of multiple players on a same game scene
+
+        if (experiment2)
+            InvokeRepeating("AddAPlayer", 10.0f, 10.0f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    //Add a player add the given gameObject to the gameScene
+
+    void AddAPlayer()
+    {
+        GameObject player = Instantiate(playerObj);
+    }
+}
